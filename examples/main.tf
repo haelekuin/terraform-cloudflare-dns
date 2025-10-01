bd8fef3fd8e0f430996ef0a0f80013f0b07fb568
haele <haele@catboys.cafe>
2025-10-01 12:50:24 +0200

    docs: add example

---
+++ examples/main.tf
@@ -0,0 +1,38 @@
+module "dns" {
+  source  = "haelekuin/cloudflare/dns"
+  version = "~> 0.1.0"
+  zone_id = "m30w"
+  records = {
+    a = {
+      name    = "cats.meow.com"
+      type    = "A"
+      content = "1.2.3.4"
+    }
+    aaaa = {
+      name    = "cats.meow.com"
+      type    = "AAAA"
+      content = "fe80::8e2d:7aff:fe3c:9a4b"
+    }
+    cname = {
+      content = "cats.meow.com"
+      name    = "meow.meow.com"
+      type    = "CNAME"
+      proxied = false
+    }
+    mx = {
+      content  = "mail.meow.com"
+      name     = "meow.com"
+      priority = 20
+      proxied  = false
+      ttl      = 1
+      type     = "MX"
+    }
+    txt = {
+      content = "meowmeowmeowmeowmeowmeow"
+      name    = "meow.com"
+      proxied = false
+      ttl     = 1
+      type    = "TXT"
+    }
+  }
+}
---
+++ examples/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.9"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = "super_securely_stored_api_token"
+}
